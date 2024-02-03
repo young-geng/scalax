32288dd8d775f07a8098666aab285e70029b2080
INE Codes <92663561+INE-KRISH@users.noreply.github.com>
2024-02-03 06:53:58 +0530

    author update setup.py
    updated ther name of the author on this setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scalax',
+    author='Xinyang Geng',
     version='0.1.1',
     license='Apache-2.0',
     description='Scaling utilities for JAX.',
